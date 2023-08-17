56dff55bcc9b43f5871116341598dd17a6dca1ef
Joi Misenti <joipolloi1@gmail.com>
2023-08-17 12:02:40 -0400

    GuessingGame works!

--- MarsWeek/src/GuessingGame.java
+++ MarsWeek/src/GuessingGame.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class GuessingGame {
 
@@ -18,6 +19,36 @@ public class GuessingGame {
         Scanner scan = new Scanner(System.in);
 
         String playerName = scan.nextLine();
-    }
 
+        System.out.println(playerName + ", I'm thinking of a number between 1 and 100.");
+        System.out.println("Try to guess my number.");
+
+        Random rand = new Random();
+
+        int randNumber = rand.nextInt(100);
+
+        System.out.print("Your guess? ");
+        int guess = scan.nextInt();
+        int countGuesses = 1;
+
+        if (guess != randNumber) {
+            while (guess != randNumber) {
+                countGuesses++;
+                if (guess < randNumber) {
+                    System.out.println("Your guess is too low, try again.");
+                    System.out.print("Your guess? ");
+                    guess = scan.nextInt();
+                } else if (guess > randNumber) {
+                    System.out.println("Your guess is too high, try again.");
+                    System.out.print("Your guess? ");
+                    guess = scan.nextInt();
+                }
+            }
+        } else {
+            System.out.println("Well done, " + playerName + "! You found my number in " + countGuesses + " tries!");
+        }
+        if (guess == randNumber) {
+            System.out.println("Well done, " + playerName + "! You found my number in " + countGuesses + " tries!");
+        }
+    }
 }
