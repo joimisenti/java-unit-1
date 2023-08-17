e49c126a5b77163b62598bef2c268225e235fc54
Joi Misenti <joipolloi1@gmail.com>
2023-08-16 21:07:57 -0400

    working on GuessingGame

---
+++ MarsWeek/src/GuessingGame.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class GuessingGame {
+    public Constructor(String someText, double someDecimal, int someNumber) {
+        this.someText = someText;
+        this.someDecimal = someDecimal;
+        this.someNumber = someNumber;
+    }
+
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("Howdy, what's your name?");
+    System.out.printlin("(type in your name)");
+    String playerName = scan.nextLine();
+
+}
